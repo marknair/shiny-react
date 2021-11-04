7eea64458f9baf61caf98c5077b11a97ab6b1976
Mark Nair <marknair@me.com>
2021-11-04 14:46:37 -0500

    First react setup

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>November 4, 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$254.00</div>
+            </div>
+        </div>)
+}
+
+export default ExpenseItem;
